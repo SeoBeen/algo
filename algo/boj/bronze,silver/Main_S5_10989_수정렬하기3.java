2d8e061175f4d21e579664aa94d5a7d3d3be54ea
jya3392 <jya3392@naver.com>
2021-10-25 20:58:35 +0900

    daily algo 20211025

---
+++ algo/boj/bronze,silver/Main_S5_10989_수정렬하기3.java
@@ -0,0 +1,25 @@
+package study.boj.prepareExam;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_S5_10989_수정렬하기3 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] cnt = new int[10001];
+		
+		for(int i = 0; i<N; i++) {
+			cnt[Integer.parseInt(br.readLine())]++;
+		}
+		StringBuilder sb = new StringBuilder();
+		for(int i = 1; i<10001; i++) {
+			while(cnt[i] > 0) {
+				sb.append(i).append("\n");
+				cnt[i]--;
+			}
+		}
+		System.out.println(sb);
+	}
+}
