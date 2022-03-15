1d3873287634dbdd4227b7cae63b79f3159f56e6
seobeen <jya3392@naver.com>
2022-03-15 23:04:24 +0900

    daily algo 20220315

--- algo/boj/bronze,silver/Main_S1_1149_RGB거리.java
+++ algo/boj/bronze,silver/Main_S1_1149_RGB거리.java
@@ -1,30 +1,35 @@
-package com.ssafy.webex.algo.boj;
+package nprpractice;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main_S1_1149_RGB거리 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 		int[][] map = new int[N+1][3];
-//		비용 저장할 배열
-		int[][] cost = new int[N+1][3];
-		for(int i = 1; i <= N; i++) {
-			for(int j = 0; j<3; j++) {
-				map[i][j]= sc.nextInt();
+		int[][] dp = new int[N+1][3];
+		for(int i = 1; i<= N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < 3; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		for(int i = 1; i<= N; i++) {
-//			red
-			cost[i][0] = Math.min(cost[i-1][1],cost[i-1][2]) + map[i][0];
-//			green
-			cost[i][1] = Math.min(cost[i-1][0],cost[i-1][2]) + map[i][1];
-//			blue
-			cost[i][2] = Math.min(cost[i-1][0],cost[i-1][1]) + map[i][2];			
-			
+		
+		for(int i = 1; i <= N; i++) {
+			// red
+			dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + map[i][0];
+			// green
+			dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + map[i][1];
+			// blue
+			dp[i][2] = Math.min(dp[i-1][1], dp[i-1][0]) + map[i][2];
 		}
-		System.out.println(Math.min(Math.min(cost[N][0],cost[N][1]), cost[N][2]));
-		sc.close();
+		
+		System.out.println(Math.min(Math.min(dp[N][0],dp[N][1]),dp[N][2]));
+		
 	}
+
 }
