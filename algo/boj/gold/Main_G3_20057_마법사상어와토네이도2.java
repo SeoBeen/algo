c33656c1429b42762f91e5b49faa71e293a3d04f
seobeen <jya3392@naver.com>
2022-04-28 09:33:23 +0900

    daily algo 20220428

--- algo/boj/gold/Main_G3_20057_마법사상어와토네이도2.java
+++ algo/boj/gold/Main_G3_20057_마법사상어와토네이도2.java
@@ -10,11 +10,19 @@ public class Main_G3_20057_마법사상어와토네이도2 {
 	//						   좌, 하, 우, 상
 	private static int[] dr = { 0,  1,  0, -1};
 	private static int[] dc = {-1,  0,  1,  0};
+	private static int[] percent = {1,1,2,2,7,7,5,10,10};
 	
-	//							상, 상상, 우상, 우하, 하, 하하, 좌하, 좌, 좌좌, 좌상, 우, 우우
-	private static int[] sdr = { -1,  -2,   -1,   1,   1,  2,    1,   0,    0 ,  -1,   0,  0 };
-	private static int[] sdc = { 0,   0,    1,    1,   0,  0 ,   -1,  -1,   -2,  -1,   1,  2 };
-	private static int N, ans;
+	private static int[][] sdr = { {1, -1, 2, -2, 1, -1, 0, -1, 1}, // 좌			
+								   {-1,-1,0,0,0,0,2,1,1},		// 하								   
+									{-1,1,2,-2,-1,1,0,-1,1},	// 우
+									{1,1,0,0,0,0,-2,-1,-1}};	// 상
+	
+	private static int[][] sdc = { {1, 1, 0, 0, 0, 0, -2, -1, -1},			
+									{-1,1,-2,2,-1,1,0,-1,1},									
+									{-1,-1,0,0,0,0,2,1,1},
+									{-1,1,-2,2,-1,1,0,-1,1}};
+	
+	private static int N, ans = 0;
 	private static int[][] map;
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,9 +36,6 @@ public class Main_G3_20057_마법사상어와토네이도2 {
 			}
 		}
 		
-		for(int[] q : map) {
-			System.out.println(Arrays.toString(q));
-		}
 		move();
 		System.out.println(ans);
 	}
@@ -39,11 +44,9 @@ public class Main_G3_20057_마법사상어와토네이도2 {
 		// 중앙 값
 		int r = N/2; int c = N/2;
 		
-		int[][] temp = new int[N][N];
-		temp[r][c] = 1;
 		int d = 0;
-		int startNum = 2;
 		int size = 1;
+		int startNum = 2;
 		while(true) {
 			int two = 2;
 			while(two-- > 0) {
@@ -51,214 +54,57 @@ public class Main_G3_20057_마법사상어와토네이도2 {
 				for(int i = 0; i < size; i++) {
 					r += dr[d];
 					c += dc[d];
-					System.out.println(startNum);
+					
 					if(!isIn(r,c)) break;
 					startNum++;
+					// 0 이면 날려갈 모래가 없다.
+					if(map[r][c] == 0) continue;
 					
 					int nowSand = map[r][c];
-					if(nowSand == 0) continue;
-					System.out.println("이동전 현재 모래 값 : "+nowSand);
-					for(int[] q : map) {
-						System.out.println(Arrays.toString(q));
-					}
+					int outSand = 0;
 					map[r][c] = 0;
-					if(d == 0) {
-						//							0    1    2     3     4    5     6    7     8     9
-						//							상, 상상, 우상, 우하, 하, 하하, 좌하, 좌, 좌좌, 좌상
-						for(int wayIdx = 0; wayIdx < 10; wayIdx++) {
-							
-							int nr = r + sdr[wayIdx];
-							int nc = c + sdc[wayIdx];
-							
-							int moveSand = 0;
-							// 상, 하
-							if(wayIdx == 0 || wayIdx == 4) {
-								moveSand = (int) Math.floor(nowSand * (double)0.07);
-							}
-							// 상상, 하하
-							else if(wayIdx == 1 || wayIdx == 5) {
-								moveSand = (int) Math.floor(nowSand * (double) 0.02);
-							}
-							// 우상, 우하
-							else if(wayIdx == 2 || wayIdx == 3) {
-								moveSand = (int) Math.floor(nowSand * (double) 0.01);
-							}
-							// 좌하, 좌상
-							else if(wayIdx == 6 || wayIdx == 9) {
-								moveSand = (int) Math.floor(nowSand * (double) 0.1);
-							}
-							// 좌좌
-							else if(wayIdx == 8) {
-								moveSand = (int) Math.floor(nowSand * (double) 0.05);
-							}
-							else { // wayIdx == 7
-								moveSand = nowSand - (int) Math.floor(nowSand *(double)  0.45);
-							}
-							System.out.println("moveSand : "+ moveSand);
-							// 범위 벗어나면 값만큼 더하기
-							if(!isIn(nr,nc)) {		
-								System.out.println("지금 더해진다");
-								System.out.println(moveSand);
-								ans += moveSand;
-								continue;
-							}
-							
-							// 그게 아니면
+					// 바람 날리기
+					for(int idx = 0; idx < 9; idx++) {
+						
+						// 바람으로 날려가는 좌표값
+						int nr = r + sdr[d][idx];
+						int nc = c + sdc[d][idx];
+						
+						// 날려갈 모래 값
+						int moveSand = (int) Math.floor( nowSand * (double) percent[idx] / 100);
+						
+						// 맵 밖으로 나가면 해당 값 만큼 나간거에 더해주기
+						if(!isIn(nr,nc)) {
+							ans += moveSand;
+						}
+						// 맵밖으로 나가지 않으면 해당 좌표로 추가
+						else {
 							map[nr][nc] += moveSand;
 						}
+						outSand += moveSand;
+						
 					}
-					else if (d == 1) {
-						//							0    1    2     3     4    5     6    7     8     9   10   11
-						//							상, 상상, 우상, 우하, 하, 하하, 좌하, 좌, 좌좌, 좌상, 우, 우우
-						for(int wayIdx = 2; wayIdx < 12; wayIdx++) {
-							
-							int nr = r + sdr[wayIdx];
-							int nc = c + sdc[wayIdx];
-							
-							int moveSand = 0;
-							// 좌, 우
-							if(wayIdx == 7 || wayIdx == 10) {
-								moveSand = (int) Math.floor(nowSand * (double) 0.07);
-							}
-							// 좌좌, 우우
-							else if(wayIdx == 8 || wayIdx == 11) {
-								moveSand = (int) Math.floor(nowSand * (double) 0.02);
-							}
-							// 좌상, 우상
-							else if(wayIdx == 2 || wayIdx == 9) {
-								moveSand = (int) Math.floor(nowSand * (double) 0.01);
-							}
-							// 좌하, 우하
-							else if(wayIdx == 6 || wayIdx == 3) {
-								moveSand = (int) Math.floor(nowSand * (double) 0.1);
-							}
-							// 하하
-							else if(wayIdx == 5) {
-								moveSand = (int) Math.floor(nowSand * (double) 0.05);
-							}
-							else { // wayIdx == 4 하
-								moveSand = nowSand - (int) Math.floor(nowSand * (double) 0.45);
-							}
-							System.out.println("moveSand : "+ moveSand);
-							// 범위 벗어나면 값만큼 더하기
-							if(!isIn(nr,nc)) {			
-								System.out.println("지금 더해진다");
-								System.out.println(moveSand);
-								ans += moveSand;
-								continue;
-							}
-							
-							// 그게 아니면
-							map[nr][nc] += moveSand;
-						}
+					// 다 돌고 남은 a에 들어갈 모래
+					int nr = r + dr[d];
+					int nc = c + dc[d];
+					// 범위 벗어나면 전체에 더하고
+					if(!isIn(nr,nc)) {
+						ans += (nowSand - outSand);
 					}
-					else if (d == 2) {
-						//							0    1    2     3     4    5     6    7     8     9   10   11
-						//							상, 상상, 우상, 우하, 하, 하하, 좌하, 좌, 좌좌, 좌상, 우, 우우
-						for(int wayIdx = 0; wayIdx < 12; wayIdx++) {
-							if(wayIdx == 7 || wayIdx == 8) continue;
-							int nr = r + sdr[wayIdx];
-							int nc = c + sdc[wayIdx];
-							
-							int moveSand = 0;
-							// 상, 하
-							if(wayIdx == 0 || wayIdx == 4) {
-								moveSand = (int) Math.floor(nowSand * (double) 0.07);
-							}
-							// 상상, 하하
-							else if(wayIdx == 1 || wayIdx == 5) {
-								moveSand = (int) Math.floor(nowSand * (double) 0.02);
-							}
-							// 좌상, 좌하
-							else if(wayIdx == 6 || wayIdx == 9) {
-								moveSand = (int) Math.floor(nowSand * (double) 0.01);
-							}
-							// 우상, 우하
-							else if(wayIdx == 2 || wayIdx == 3) {
-								moveSand = (int) Math.floor(nowSand * (double) 0.1);
-							}
-							// 우우
-							else if(wayIdx == 11) {
-								moveSand = (int) Math.floor(nowSand * (double) 0.05);
-							}
-							else { // wayIdx == 10 우
-								moveSand = nowSand - (int)Math.floor(nowSand * (double) 0.45);
-							}
-							System.out.println("moveSand : "+ moveSand);
-							// 범위 벗어나면 값만큼 더하기
-							if(!isIn(nr,nc)) {			
-								System.out.println("지금 더해진다");
-								System.out.println(moveSand);
-								ans += moveSand;
-								continue;
-							}
-							
-							// 그게 아니면
-							map[nr][nc] += moveSand;
-						}
-					}
-					else if (d == 3) {
-						//							0    1    2     3     4    5     6    7     8     9   10   11
-						//							상, 상상, 우상, 우하, 하, 하하, 좌하, 좌, 좌좌, 좌상, 우, 우우
-						for(int wayIdx = 0; wayIdx < 12; wayIdx++) {
-							if(wayIdx == 4 || wayIdx == 5) continue;
-							int nr = r + sdr[wayIdx];
-							int nc = c + sdc[wayIdx];
-							
-							int moveSand = 0;
-							// 좌, 우
-							if(wayIdx == 7 || wayIdx == 10) {
-								moveSand = (int) Math.floor(nowSand * (double) 0.07);
-							}
-							// 좌좌, 우우
-							else if(wayIdx == 8 || wayIdx == 11) {
-								moveSand = (int) Math.floor(nowSand * (double) 0.02);
-							}
-							// 좌상, 우상
-							else if(wayIdx == 2 || wayIdx == 9) {
-								moveSand = (int) Math.floor(nowSand * (double) 0.1);
-							}
-							// 좌하, 우하
-							else if(wayIdx == 6 || wayIdx == 3) {
-								moveSand = (int) Math.floor(nowSand * (double) 0.01);
-							}
-							// 상상
-							else if(wayIdx == 1) {
-								moveSand = (int) Math.floor(nowSand * (double) 0.05);
-							}
-							else { // wayIdx == 0 상
-								moveSand = nowSand - (int)Math.floor(nowSand * (double) 0.45);
-							}
-							System.out.println("moveSand : "+ moveSand);
-							// 범위 벗어나면 값만큼 더하기
-							if(!isIn(nr,nc)) {			
-								System.out.println("지금 더해진다");
-								System.out.println(moveSand);
-								ans += moveSand;
-								continue;
-							}
-							
-							// 그게 아니면
-							map[nr][nc] += moveSand;
-						}
+					else {
+						// 그게아니면 해당좌표에 더해주기
+						map[nr][nc] += (nowSand - outSand);
 					}
 					
-					System.out.println("이동후");
-					for(int[] q : map) {
-						System.out.println(Arrays.toString(q));
-					}
-				}
+//					System.out.println("나간 모래양 : "+ans + " r : " + r + " c : " + c);
+					if(r == 0 && c == 0) return;
+				} // for
+//				System.out.println(d);
 				d = (d + 1) % 4;
-			}
+			} // while(two)
 			size++;
-			
 			if(startNum >= N * N) break;
-		}
-		
-//		for(int[] q : map) {
-//			System.out.println(Arrays.toString(q));
-//		}
-		
+		} // while
 	}
 
 	private static boolean isIn(int r, int c) {
